84a0a13d8bf72ee4e23b176593654e886d55e1ca
zh2x <doe_cn@qq.com>
2015-11-08 00:06:29 +0800

    Update Constants.m
    Modify this demo source github link.

--- SpO2-BLE-for-iOS/Constants.m
+++ SpO2-BLE-for-iOS/Constants.m
@@ -12,4 +12,4 @@ NSString *UUID_SERVICE_DATA             = @"49535343-fe7d-4ae5-8fa9-9fafd205e455
 NSString     *UUID_CHARACTER_RECEIVE    = @"49535343-1e4d-4bd9-ba61-23c647249616";
 NSString     *UUID_MODIFY_BT_NAME       = @"00005343-0000-1000-8000-00805F9B34FB";
 
-NSString *SOURCE_CODE_SITE              = @"https://www.github.com/zh2x/SpO2_BLE";
+NSString *SOURCE_CODE_SITE              = @"https://github.com/zh2x/SpO2-BLE-for-iOS";
